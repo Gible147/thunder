eef8cc8d29aa3b760adc8f544c4a68766b89c897
Nico <nico.kichukov@outlook.com>
2021-06-02 09:25:43 -0400

    Currently launches Minecraft client

---
+++ src/main/java/com/gible147/thunder/Thunder.java
@@ -0,0 +1,12 @@
+package com.gible147.thunder;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Thunder implements ModInitializer {
+
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/com/gible147/thunder/registry/ModItems.java
@@ -0,0 +1,16 @@
+package com.gible147.thunder.registry;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+
+import java.rmi.registry.Registry;
+
+public class ModItems {
+
+    public static final Item LIGHTNING_BOW = new Item(new Item.Settings().group(ItemGroup.COMBAT));
+
+    public static void registerItems() {
+        Registry.register(Registry.)
+    }
+
+}
