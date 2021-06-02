9fc7a0927b97277252c3dd408e77ac12e72ac897
Nico <nico.kichukov@outlook.com>
2021-06-02 11:45:17 -0400

    Added model/texture for lightning_bow.json and added it into the game, no actual functions yet, and no direct name.

--- src/main/java/com/gible147/thunder/Thunder.java
+++ src/main/java/com/gible147/thunder/Thunder.java
@@ -1,12 +1,14 @@
 package com.gible147.thunder;
 
+import com.gible147.thunder.registry.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 public class Thunder implements ModInitializer {
 
+    public static final String MOD_ID = "thunder";
 
     @Override
     public void onInitialize() {
-
+        ModItems.registerItems();
     }
 }
--- src/main/java/com/gible147/thunder/registry/ModItems.java
+++ src/main/java/com/gible147/thunder/registry/ModItems.java
@@ -1,16 +1,20 @@
 package com.gible147.thunder.registry;
 
+import com.gible147.thunder.Thunder;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
-
-import java.rmi.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class ModItems {
 
     public static final Item LIGHTNING_BOW = new Item(new Item.Settings().group(ItemGroup.COMBAT));
 
     public static void registerItems() {
-        Registry.register(Registry.)
+        Registry.register(Registry.ITEM, new Identifier(Thunder.MOD_ID, "lightning_bow"), LIGHTNING_BOW);
     }
+
+
+
 
 }
