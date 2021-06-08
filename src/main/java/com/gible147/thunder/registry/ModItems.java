d1ead6281599a7c5478cd8cab4455ec547d0ce18
timrolsh <timrol24@bergen.org>
2021-06-08 14:37:52 -0400

    added new item, thunder staff to the game

--- src/main/java/com/gible147/thunder/registry/ModItems.java
+++ src/main/java/com/gible147/thunder/registry/ModItems.java
@@ -15,11 +15,11 @@ public class ModItems {
 
     public static final Item LIGHTNING_BOW = new BowItem(new Item.Settings().group(Thunder.ITEM_GROUP));
     public static final BlockItem LIGHTNING_STONE  = new BlockItem(ModBlocks.LIGHTNING_STONE, new Item.Settings().group(Thunder.ITEM_GROUP));
-
-
+    public static final Item THUNDER_STAFF = new Item(new Item.Settings().group(Thunder.ITEM_GROUP));
     public static void registerItems() {
         Registry.register(Registry.ITEM, new Identifier(Thunder.MOD_ID, "lightning_bow"), LIGHTNING_BOW);
         Registry.register(Registry.ITEM, new Identifier(Thunder.MOD_ID, "lightning_stone"), LIGHTNING_STONE);
+        Registry.register(Registry.ITEM, new Identifier(Thunder.MOD_ID, "thunder_staff"), THUNDER_STAFF);
     }
 
 
