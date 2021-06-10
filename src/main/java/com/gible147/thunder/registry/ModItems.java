a06f611655c028744666401bffb48d4bd43418be
Nico <nico.kichukov@outlook.com>
2021-06-10 14:52:01 -0400

    updated loot tables

--- src/main/java/com/gible147/thunder/registry/ModItems.java
+++ src/main/java/com/gible147/thunder/registry/ModItems.java
@@ -13,7 +13,6 @@ public class ModItems {
     public static final Item LIGHTNING_BOW = new BowItem(new Item.Settings().group(Thunder.ITEM_GROUP).maxCount(1));
     public static final BlockItem LIGHTNING_STONE  = new BlockItem(ModBlocks.LIGHTNING_STONE, new Item.Settings().group(Thunder.ITEM_GROUP).maxCount(16));
     public static final TridentItem THUNDER_STAFF = new TridentItem(new Item.Settings().group(Thunder.ITEM_GROUP).maxCount(1));
-
     public static void registerItems() {
         Registry.register(Registry.ITEM, new Identifier(Thunder.MOD_ID, "lightning_bow"), LIGHTNING_BOW);
         Registry.register(Registry.ITEM, new Identifier(Thunder.MOD_ID, "lightning_stone"), LIGHTNING_STONE);
