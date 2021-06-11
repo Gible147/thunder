650bf63f2b10b39216d74384d5acc103bf76cfcb
Nico <nico.kichukov@outlook.com>
2021-06-11 14:49:14 -0400

    Changed a bunch of stuff :)

--- src/main/java/com/gible147/thunder/LightningBow.java
+++ src/main/java/com/gible147/thunder/LightningBow.java
@@ -1,11 +1,22 @@
 package com.gible147.thunder;
 
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.item.BowItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.world.World;
+
+import java.util.List;
 
 public class LightningBow extends BowItem{
 
     public LightningBow(Settings settings) {
         super(settings);
+    }
+
+    public void appendTooltip(ItemStack itemStack, World world, List<Text> tooltip, TooltipContext tooltipContext) {
+        tooltip.add(new TranslatableText("item.thunder.lightning_bow.tooltip"));
     }
 
 }
--- src/main/java/com/gible147/thunder/registry/ModItems.java
+++ src/main/java/com/gible147/thunder/registry/ModItems.java
@@ -4,15 +4,16 @@ import com.gible147.thunder.Thunder;
 
 import net.minecraft.item.*;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.Rarity;
 import net.minecraft.util.registry.Registry;
 
 import static com.gible147.thunder.registry.ModBlocks.LIGHTNING_STONE;
 
 public class ModItems {
 
-    public static final Item LIGHTNING_BOW = new BowItem(new Item.Settings().group(Thunder.ITEM_GROUP).maxCount(1));
-    public static final BlockItem LIGHTNING_STONE  = new BlockItem(ModBlocks.LIGHTNING_STONE, new Item.Settings().group(Thunder.ITEM_GROUP).maxCount(16));
-    public static final TridentItem THUNDER_STAFF = new TridentItem(new Item.Settings().group(Thunder.ITEM_GROUP).maxCount(1));
+    public static final Item LIGHTNING_BOW = new BowItem(new Item.Settings().group(Thunder.ITEM_GROUP).maxCount(1).rarity(Rarity.EPIC));
+    public static final BlockItem LIGHTNING_STONE  = new BlockItem(ModBlocks.LIGHTNING_STONE, new Item.Settings().group(Thunder.ITEM_GROUP).maxCount(16).rarity(Rarity.RARE));
+    public static final TridentItem THUNDER_STAFF = new TridentItem(new Item.Settings().group(Thunder.ITEM_GROUP).maxCount(1).rarity(Rarity.EPIC));
     public static void registerItems() {
         Registry.register(Registry.ITEM, new Identifier(Thunder.MOD_ID, "lightning_bow"), LIGHTNING_BOW);
         Registry.register(Registry.ITEM, new Identifier(Thunder.MOD_ID, "lightning_stone"), LIGHTNING_STONE);
